e11aa9e146575bae244928fa9fe332ea250ece3d
keshav.a <achyut2311@gmail.com>
2024-12-31 12:51:49 +0530

    fixed the problem of margin-top from every page and also added explicit css to different pages

--- js/validate.js
+++ js/validate.js
@@ -21,12 +21,13 @@ function vlad() {
 
     if (name == "") {
         alert("Please enter the name");
-
+        document.getElementById("nameInput").focus();
         return false;
 
     }
     else if (email == "") {
         alert("Please enter a valid Email");
+        document.getElementById("emailInput").focus();
         return false;
     }
 
@@ -58,16 +59,21 @@ function vladan(){
 
     if (firstName == "") {
         alert("Please enter the first name");
+        document.getElementById("fname").focus();
+
         return false;
 
     }
     else if (lastName == "") {
         alert("Please enter a valid last name");
+        document.getElementById("lname").focus();
+
         return false;
     }
 
     else if(subject == ""){
         alert("Please enter the text");
+        document.getElementById("subject").focus();
         return false;
     }
     else {
